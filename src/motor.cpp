066769505bfed82f55769e56f4714fff183b49be
Henorvell <comword@live.com>
2015-12-15 14:09:50 +0800

    add motor.cpp

--- src/motor.cpp
+++ src/motor.cpp
@@ -25,24 +25,30 @@ Motor::~Motor()
 }
 int Motor::run(std::string id,int speed)
 {
+	//FF FF (ID) 04 03 20 (0000000000000000B) SM
+	//                     | Speed  |D|
 	//speed from -1023 to 1023
 	if (abs(speed) > 1023)
 		throw std::runtime_error(std::string("Motor::run speed value larger than 1023!\n"));
 	int rid = find_motor_byid(id)->id;
-	char *buf=(char *)malloc(11*sizeof(char));
-	buf=(char *)memset(buf,0,11*sizeof(char));
+	char *buf=(char *)malloc(9*sizeof(char));
+	buf=(char *)memset(buf,0,9*sizeof(char));
 	buf=(char *)memset(buf,MOTOR_HEAD,2*sizeof(char));
+	rid &= 0xff;
+	char *motorID = (char *)&rid;
+	*(buf+2) = *motorID;
+	*(buf+3) = 0x04;
+	*(buf+4) = 0x03;
+	*(buf+5) = 0x20;
+	*(buf+6) = speed & 255;
+	*(buf+7) = (speed > 0) ? 1024 + (speed & 256) : abs(speed) & 256;
+	finish_checksum(buf,9);
 	return 0;
 }
 int Motor::run(std::string id,int speed,int ms)
 {
-	//speed from -1023 to 1023
-	if (abs(speed) > 1023)
-		throw std::runtime_error(std::string("Motor::run speed value larger than 1023!\n"));
-	int rid = find_motor_byid(id)->id;
-	char *buf=(char *)malloc(11*sizeof(char));
-	buf=(char *)memset(buf,0,11*sizeof(char));
-	buf=(char *)memset(buf,MOTOR_HEAD,2*sizeof(char));
+	this -> run (id);
+	
 	return 0;
 }
 int Motor::stop(std::string id)
@@ -63,7 +69,7 @@ void Motor::do_uart_cycle()
 }
 void Motor::set_Motor_mode(std::string id)
 {
-	//FF FF ID 07 03 06 00 00 00 00 SM
+	//FF FF (ID) 07 03 06 00 00 00 00 SM
 	int rid = find_motor_byid(id)->id;
 	char *buf=(char *)malloc(11*sizeof(char));
 	buf=(char *)memset(buf,0,11*sizeof(char));
