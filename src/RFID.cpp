5514b40ea72083fe2ad4dc6a85f8f4b8fa40838d
Henorvell Ge <comword@live.com>
2016-01-11 11:15:39 +0800

    write python.

--- src/RFID.cpp
+++ src/RFID.cpp
@@ -8,17 +8,37 @@
 #include "RFID.h"
 
 #include <stdlib.h>
+#include <vector>
 #include <string.h>
 #include <errno.h>
 #include <stdexcept>
 RFID::RFID()
 {
 	Py_Initialize();
+	if (!Py_IsInitialized())
+		throw std::runtime_error(std::string("RFID::assert !Py_IsInitialized()\n"));
 	PyRun_SimpleString("import sys");
 	PyRun_SimpleString("sys.path.append('./utility/RC522/')");
 	RC522Mod = PyImport_ImportModule("MFRC522");
+	if (RC522Mod == nullptr)
+		throw std::runtime_error(std::string("RFID::assert RC522Mod == nullptr\n"));
+	PyRun_SimpleString("import RPi.GPIO as GPIO");
+	PyRun_SimpleString("MIFAREReader = MFRC522.MFRC522()");
+	read_once = PyObject_GetAttrString(RC522Mod, "read_once");
+	if (read_once == nullptr)
+		throw std::runtime_error(std::string("RFID::assert read_once == nullptr\n"));
 }
 RFID::~RFID()
 {
+	Py_DECREF(read_once);
+	Py_DECREF(RC522Mod);
 	Py_Finalize();
+}
+std::vector<int> RFID::read_card()
+{
+	using namespace std;
+	vector<int> vec;
+	PyObject* pResult = nullptr;
+	pResult = PyEval_CallObject(read_once,nullptr);
+	return vec;
 }
--- src/RFID.h
+++ src/RFID.h
@@ -6,11 +6,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 #include <python2.7/Python.h>
+#include <vector>
 class RFID
 {
 public:
 	RFID();
 	virtual ~RFID();
+	std::vector<int> read_card();
 protected:
 	PyObject *RC522Mod = nullptr;
+	PyObject *read_once = nullptr;
 };
